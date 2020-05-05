95bf662f4c0f430590b3a6dce1c224c2309e711b
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2020-05-05 22:13:41 +0100

    "Holidays and getStaffName methods"

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const SlackBot = require('slackbots'),
 // bot name and access token
 const bot = new SlackBot({
     token: `${process.env.BOT_TOKEN}`,
-    name: 'rosterbot'
+    name: 'myroster'
   });
 
   // initialize the app
@@ -40,45 +40,47 @@ const bot = new SlackBot({
   // checks and respond to incoming messages
   function messageHandler(data){
     if(data.text.includes(' /myshifts')){
-
-      const staffUser = data.user;
-      console.log(staffUser);
-      
-      for(shift of shifts){
-        if (shift.slackUser=== staffUser) {
-          bot.postMessageToUser(shift.staff, shift.date +" "+ shift.shiftTime)
-        } else {
-          for(staff of staffList){
-            if(staff.slackUser === staffUser){
-              bot.postMessageToUser(staff.name, "No shifts found");
-            }
-          }
-        }
-      }
-        // availability();
+      myConfirmedShifts(data);
     }else if(data.text.includes(' /holidays')){
-        checkHolidays();
-    }else if(data.text.includes(' /confirmed')){
-        confirmShift();
+        checkHolidays(data);
     }else if(data.text.includes(' /help')){
       help();
     }
-
-
-function availability(){
-
-    // axios.get(`${process.env.MONGODB_TOKEN}`).then(res =>{
-    //     console.log(res.data);
-    // });    
-        const params = {
-          icon_emoji: ':question:'
-        };
-        bot.postMessageToChannel(
-          'roster',
-          ` `,
-          params
-        );
+}
+  function getStaffName(data){
+      for(staff of staffList){
+        if(staff.slackUser === data.user){
+          return staff.name;
+        }
+    }
+  }
+  function myConfirmedShifts(data){
+    const staffUser = data.user;
+      console.log(staffUser);
+      var total = 0;
+      for(shift of shifts){
+        if (shift.slackUser===staffUser & shift.confirmed===true) {
+          total += 1;
+          bot.postMessageToUser(shift.staff,
+             `Confirmed shift ${total}: 
+              ${shift.date} ${shift.shiftTime}`);          
+        }
       }
+      if(total===0){
+        bot.postMessageToUser(getStaffName(data), "No shifts found");
+        }  
+        }
+
+
+  function checkHolidays(data){
+    for(hol of holidays){
+      if(data.user === hol.slackUser){
+        bot.postMessageToUser(getStaffName(data), 
+          `Your Holidays starts at: 
+           ${hol.start} and ends: ${hol.finish}`)
+      }
+    }
+  }
 
 function help(){
   const params = {
@@ -89,12 +91,10 @@ function help(){
     `Use the following commands:
          "/available" command to let your manager know when you wanna work. 
          "/holidays" command will show when your holidays are schedule.
-         "/confirmed" will show all your confirmed shifts.`,
+         "/myshifts" will show all your confirmed shifts.`,
     params
   );
 }
-}
-
   var shifts = [
   {
     "date": "2020-05-01",
@@ -119,24 +119,23 @@ function help(){
     "slackUser": "UU98UF8AG",
     "staff": "querinosmith",
     "manager": "John Smith",
-    "confirmed": true
+    "confirmed": false
  }
- 
 ]
 
 var holidays = [
   {
-    "staff": "UU98UF8AG",
+    "slackUser": "UU98UF8AG",
     "start": "2020-08-01",
     "finish":"2020-08-30"
   },
   {
-    "staff": "U******",
+    "slackUser": "U******",
     "start": "2020-10-01",
     "finish":"2020-10-30"
   },
   {
-    "staff": "U*********",
+    "slackUser": "U*********",
     "start": "2020-12-01",
     "finish":"2020-12-30"
   }
